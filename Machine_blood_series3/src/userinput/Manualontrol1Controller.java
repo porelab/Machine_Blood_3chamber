a93c4f18161e290e4f6540974485f1050b1d7d3f
AU <AU@DESKTOP-S2GGGRR>
2021-07-02 13:09:04 +0530

    sd

---
+++ Machine_blood_series3/src/userinput/Manualontrol1Controller.java
@@ -0,0 +1,16 @@
+package userinput;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+
+public class Manualontrol1Controller implements Initializable {
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
